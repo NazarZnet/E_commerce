a32638021b158162217e46cf962f96fe56438a00
NazarZnet <nazarznetynyak123@gmail.com>
2025-01-01 18:19:22 +0200

    Init project

---
+++ frontent/src/App.tsx
@@ -0,0 +1,22 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+
+const MainPage = lazy(() => import('./pages/Main'));
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<MainPage />} />
+          
+          </Route>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
